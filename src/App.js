750e01585ff97e68bba6e62ec1e08f45ce69b016
Nahid-07 <nahidasif1998@gmail.com>
2022-10-11 03:00:00 +0600

    added header img

--- src/App.js
+++ src/App.js
@@ -4,18 +4,22 @@ import Layout from './Components/Layout/Layout';
 import Topics from './Components/Topics';
 import Statistics from './Components/Statistics';
 import Blog from './Blog';
+import ErrorPage from './Components/ErrorPage';
 
 function App() {
   const router = createBrowserRouter([
     {
       path:'/', element:<Layout></Layout>,
+      errorElement:<ErrorPage></ErrorPage>,
       children:[
         {
-          path:'/', element:<Topics />
+          path:'/',
+          loader: () => fetch('https://openapi.programming-hero.com/api/quiz'),
+          element:<Topics />
         },
         {
           path:'/topics', element:<Topics />,
-          loader: () => fetch('https://openapi.programming-hero.com/api/quiz')
+          loader: () => fetch('https://openapi.programming-hero.com/api/quiz'),
         },
         {
           path:'/statistics', element:<Statistics />
--- src/Components/Topics.js
+++ src/Components/Topics.js
@@ -1,20 +1,37 @@
-import React from 'react';
-import { useLoaderData } from 'react-router-dom';
-import QuizTopic from './QuizTopic';
+import React from "react";
+import { useLoaderData } from "react-router-dom";
+import QuizTopic from "./QuizTopic";
+import Image from './Image/bg.jpg'
 
 const Topics = () => {
-    const topics = useLoaderData();
-    const {data} = topics
-    console.log(data)
-    return (
-        <div>
-            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-5 mx-5'>
-                {
-                    data.map(quizTopic => <QuizTopic key={quizTopic.id} quizTopic={quizTopic}></QuizTopic>)
-                }
-            </div>
+  const topics = useLoaderData();
+  // const {data} = topics
+  console.log(topics);
+  return (
+    <div>
+      <div
+        className="hero min-h-screen"
+        style={{ backgroundImage:`url(${Image})` }}
+      >
+        <div className="hero-overlay bg-opacity-60"></div>
+        <div className="hero-content text-center text-neutral-content">
+          <div className="max-w-md">
+            <h1 className="mb-5 text-5xl font-bold">Are you a Learner ?</h1>
+            <p className="mb-5">
+              You Want to develop your knowledge? well you are in right place!!
+              so What are you waiting for!!!! 
+            </p>
+            <button className="btn btn-primary">Get Started</button>
+          </div>
         </div>
-    );
+      </div>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-5 mx-5">
+        {topics.data.map((quizTopic) => (
+          <QuizTopic key={quizTopic.id} quizTopic={quizTopic}></QuizTopic>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Topics;
