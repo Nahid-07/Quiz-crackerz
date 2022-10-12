1707f3602f742184426c92e2abd0e2d1b5e42e81
Nahid-07 <nahidasif1998@gmail.com>
2022-10-12 11:39:31 +0600

    fiexed some bugs

--- src/Components/Quizes.js
+++ src/Components/Quizes.js
@@ -6,7 +6,7 @@ import Quiz from './Quiz';
 const Quizes = () => {
     const quizQuestion = useLoaderData().data;
     const {questions,name,id} = quizQuestion;
-    console.log(quizQuestion)
+    // console.log(quizQuestion)
     return (
         <div className='bg-gray-300'>
             <h1 className='text-center text-3xl md:text-5xl py-5 font-semibold text-slate-900'>Quiz for {name}</h1>
